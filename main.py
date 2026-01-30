1ca27ced74b34cbb5e6d592250f337ddd14ddbd8
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-01-30 16:01:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# 4
+items = [1, 2, 2, 3, 3, 4, 1]
+
+unique_items = list(set(items))
+
+
+unique_items.sort()
+
+
+first_element = unique_items[0]
+last_element = unique_items[-1]
+
+
+length = len(unique_items)
+
+
+print("Natija:", unique_items)
+print("Birinchi element:", first_element)
+print("Oxirgi element:", last_element)
+print("Ro‘yxat uzunligi:", length)
